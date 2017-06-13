2922ac1f59b66f856032ed50057891a57a22c814
skywind3000 <skywind3000@163.com>
2017-06-13 23:39:10 +0800

    commit new inetkcp.c

--- system/inetkcp.c
+++ system/inetkcp.c
@@ -578,7 +578,7 @@ int ikcp_input(ikcpcb *kcp, const char *data, long size)
 		ikcp_log(kcp, IKCP_LOG_INPUT, "[RI] %d bytes", size);
 	}
 
-	if (data == NULL || size < 24) return -1;
+	if (data == NULL || size < (int)IKCP_OVERHEAD) return -1;
 
 	while (1) {
 		IUINT32 ts, sn, len, una, conv;
