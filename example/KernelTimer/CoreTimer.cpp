93c71bfda1998804d269cee2d800b7c25b805f9f
skywind3000 <skywind3000@163.com>
2021-12-02 08:40:59 +0800

    commit new CoreTimer.h

---
+++ example/KernelTimer/CoreTimer.cpp
@@ -0,0 +1,133 @@
+//=====================================================================
+//
+// CoreTimer.cpp - 
+//
+// Created by skywind on 2021/12/01
+// Last Modified: 2021/12/01 17:44:14
+//
+//=====================================================================
+
+#include <stddef.h>
+#include <assert.h>
+
+#include "CoreTimer.h"
+
+
+//---------------------------------------------------------------------
+// ctor
+//---------------------------------------------------------------------
+Timer::Timer()
+{
+	_sched = NULL;
+	_inited = false;
+	_cb = NULL;
+	user = NULL;
+	itimer_evt_init(&_evt, callback, this, NULL);
+}
+
+
+//---------------------------------------------------------------------
+// dtor
+//---------------------------------------------------------------------
+Timer::~Timer()
+{
+	stop();
+	user = NULL;
+}
+
+
+//---------------------------------------------------------------------
+// initialize scheduler and callback
+//---------------------------------------------------------------------
+void Timer::init(Scheduler *sched, TimerCallback cb)
+{
+	stop();
+	_sched = sched;
+	_cb = cb;
+	_inited = true;
+}
+
+
+//---------------------------------------------------------------------
+// callback
+//---------------------------------------------------------------------
+void Timer::callback(void *obj, void *user)
+{
+	Timer *self = (Timer*)obj;
+	if (self->_cb) {
+		self->_cb(self);
+	}
+}
+
+
+//---------------------------------------------------------------------
+// start timer
+//---------------------------------------------------------------------
+bool Timer::start(uint32_t period, int repeat)
+{
+	assert(_inited == true);
+	assert(_sched != NULL);
+	if (_inited == false || _sched == NULL) {
+		return false;
+	}
+	if (_sched->_inited == false) {
+		return false;
+	}
+	itimer_evt_start(&(_sched->_mgr), &_evt, period, repeat);
+	return true;
+}
+
+
+//---------------------------------------------------------------------
+// stop timer
+//---------------------------------------------------------------------
+void Timer::stop()
+{
+	assert(_inited == true);
+	itimer_evt_stop(&(_sched->_mgr), &_evt);
+}
+
+
+//---------------------------------------------------------------------
+// check is running
+//---------------------------------------------------------------------
+bool Timer::is_running() const
+{
+	if (_inited) {
+		return itimer_evt_status(&_evt)? true : false;
+	}
+	return false;
+}
+
+
+//---------------------------------------------------------------------
+// ctor
+//---------------------------------------------------------------------
+Scheduler::Scheduler()
+{
+	_inited = false;
+	itimer_mgr_init(&_mgr, 0, 10);
+}
+
+//---------------------------------------------------------------------
+// dtor
+//---------------------------------------------------------------------
+Scheduler::~Scheduler()
+{
+	itimer_mgr_destroy(&_mgr);
+}
+
+
+//---------------------------------------------------------------------
+// init
+//---------------------------------------------------------------------
+void Scheduler::init(uint32_t current, uint32_t interval)
+{
+	if (_inited) {
+		itimer_mgr_destroy(&_mgr);
+	}
+	itimer_mgr_init(&_mgr, current, interval);
+	_inited = true;
+}
+
+
---
+++ example/KernelTimer/CoreTimer.h
@@ -0,0 +1,91 @@
+//=====================================================================
+//
+// CoreTimer.h - 
+//
+// Created by skywind on 2021/12/01
+// Last Modified: 2021/12/01 17:44:07
+//
+//=====================================================================
+#ifndef _CORE_TIMER_H_
+#define _CORE_TIMER_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <unordered_map>
+#include <functional>
+
+#include "../../system/itimer.h"
+
+
+//---------------------------------------------------------------------
+// Declaration
+//---------------------------------------------------------------------
+class Scheduler;
+class Timer;
+
+
+//---------------------------------------------------------------------
+// Callback
+//---------------------------------------------------------------------
+typedef std::function<void(Timer*)> TimerCallback;
+
+
+//---------------------------------------------------------------------
+// Timer
+//---------------------------------------------------------------------
+class Timer
+{
+public:
+	virtual ~Timer();
+	Timer();
+
+public:
+
+	void init(Scheduler *sched, TimerCallback cb);
+
+	bool start(uint32_t period, int repeat = 0);
+
+	void stop();
+
+	bool is_running() const;
+
+public:
+	void *user;
+
+protected:
+	static void callback(void *obj, void *user);
+
+protected:
+	Scheduler *_sched;
+	bool _inited;
+	TimerCallback _cb;
+	itimer_evt _evt;
+};
+
+
+//---------------------------------------------------------------------
+// Scheduler
+//---------------------------------------------------------------------
+class Scheduler
+{
+public:
+	virtual ~Scheduler();
+	Scheduler();
+
+public:
+
+	void init(uint32_t current, uint32_t interval = 5);
+
+private:
+	friend Timer;
+	bool _inited;
+	itimer_mgr _mgr;
+};
+
+
+
+#endif
+
+
+
+
